82a09e8bf73de83eff8592c2a31fd4665a305902
biggestbarnacle1 <52912384+biggestbarnacle1@users.noreply.github.com>
2019-07-15 12:32:02 -0600

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function getrandom() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for (var i = 0; i < 5; i++)
+    for (var i = 0; i < 20; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
     return text;
 }
