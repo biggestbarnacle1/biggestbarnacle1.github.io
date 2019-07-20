5849b144ca13efe43560bdade5ed1abd2b8510c7
biggestbarnacle1 <52912384+biggestbarnacle1@users.noreply.github.com>
2019-07-20 13:26:53 -0600

    Update

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-
 var endpoint = "https://www.jsonstore.io/5234f7b5ed523f406fa03b5f254bee8d318f04f2ceac974f32c01103faad58a7";
 
 function geturl(){
-    var url = document.getElementById("urlinput").value;
-	var url2 = document.getElementById("urlinput2").value;
-	var url3 = document.getElementById("urlinput3").value;
-   var url_M = [url, url2, url3];
-	return url_M;
+    var lines = $('textarea').val().split('\n');
+	return lines;
 }
 
 function getrandom() {
@@ -24,40 +20,37 @@ function genhash(){
     }
 }
 
-function send_request(url) {
-	url1 = url[0];
-	url2 = url[1];
-	url3 = url[2];
-    this.url1 = url1;
-this.url2 = url2;
-this.url3 = url3;
+function send_request(lines) {
+
     $.ajax({
         'url': endpoint + "/" + window.location.hash.substr(1),
         'type': 'POST',
-        'data': JSON.stringify(this.url1),
+        'data': JSON.stringify(this.lines.length),
         'dataType': 'json',
         'contentType': 'application/json; charset=utf-8'
 })
-$.ajax({
-        'url': endpoint + "/" + window.location.hash.substr(2),
-        'type': 'POST',
-        'data': JSON.stringify(this.url2),
-        'dataType': 'json',
-        'contentType': 'application/json; charset=utf-8'
-})
-$.ajax({
-        'url': endpoint + "/" + window.location.hash.substr(3),
-        'type': 'POST',
-        'data': JSON.stringify(this.url3),
-        'dataType': 'json',
-        'contentType': 'application/json; charset=utf-8'
-})
+    for(var i = 0;i < lines.length;i++){
+        my_url = lines[i];
+        this.my_url = lines[i];
+        $.ajax({
+            'url': endpoint + "/" + window.location.hash.substr(i+1) + "-" + (i+1),
+            'type': 'POST',
+            'data': JSON.stringify(this.my_url),
+            'dataType': 'json',
+            'contentType': 'application/json; charset=utf-8'
+    })
+    }
 }
 
 function shorturl(){
     var urls = geturl();
+    if (urls == "") {
+        alert("No links entered!")
+    }
+    else {
     genhash();
     send_request(urls);
+    }
 }
 
 function getRandomInt(min, max) {
@@ -66,11 +59,17 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-var hashh = window.location.hash.substr(getRandomInt(1,4));
-
 if (window.location.hash != "") {
+    var hashh_number = window.location.hash.substr(1);
 
-    $.getJSON(endpoint + "/" + hashh, function (data) {
+    $.getJSON(endpoint + "/" + hashh_number, function (data) {
+        data = data["result"];
+
+    });
+    random_number = getRandomInt(1,data);
+    var hashh = window.location.hash.substr(random_number);
+
+    $.getJSON(endpoint + "/" + hashh + "-" + random_number, function (data) {
         data = data["result"];
 
         if (data != null) {
